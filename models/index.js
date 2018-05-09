c1f62ac87a8a67c0fe5fdea9e9088b8c82f8190f
jaime franco cerritos <jfcslc@outlook.com>
2018-05-09 02:00:42 -0600

    connection

--- models/index.js
+++ models/index.js
@@ -35,3 +35,18 @@ db.Sequelize = Sequelize;
 
 module.exports = db;
 
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "cangetin",
+    database: "burgers_db"
+  });
+};
+
+
+
+
